ef87a478b1e9962e46a45099bd5bd3dabaacaf86
Joseph CG <josephcg43@gmail.com>
2022-04-23 14:57:41 +0100

    Add gpu explanation

--- Code/Marching_Cubes/Shaders/Compute/generatedistance.glsl
+++ Code/Marching_Cubes/Shaders/Compute/generatedistance.glsl
@@ -8,14 +8,14 @@ layout(binding=1) buffer Grid {
 layout(local_size_x=8,local_size_y=8,local_size_z=8) in;
 
 void generate(uvec3 gid) {
-    uint arrID = getArrID(gid);
-    distanceValues[arrID] = modified_distance(gid*chunkStride + chunkPosition);
+    uint arrID = getArrID(gid); //get the index into the buffer
+    distanceValues[arrID] = modified_distance(gid*chunkStride + chunkPosition); //call the SDF at the corresponding world-space location
 }
 
 void main() {
 	uvec3 gid = gl_GlobalInvocationID;
 	if (gid.x > chunkSize.x || gid.y > chunkSize.y || gid.z > chunkSize.z) {
-		return;
+		return; //do not try to compute the SDF if gid is outside of the volume
 	}
 	generate(gid);
 }
